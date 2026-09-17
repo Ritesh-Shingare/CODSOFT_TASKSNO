0bf2cdf54cad3985158ee6f649ab8481a0524815
Ritesh Shingare <riteshshingare039@gmail.com>
2026-09-17 22:44:34 +0530

    feat: add contact form validation

--- TASK-1-Personal-Portfolio/script.js
+++ TASK-1-Personal-Portfolio/script.js
@@ -19,3 +19,49 @@ navLinks.forEach((link) => {
         menuToggle.textContent = "☰";
     });
 });
+
+const contactForm = document.querySelector("#contactForm");
+
+contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    document.querySelector("#form-status").textContent = "";
+
+    const name = document.querySelector("#name");
+    const email = document.querySelector("#email");
+    const message = document.querySelector("#message");
+
+    const errorMessages = document.querySelectorAll(".error-message");
+
+    errorMessages.forEach((error) => {
+        error.textContent = "";
+    });
+
+    let isValid = true;
+
+    if (name.value.trim() === "") {
+        name.nextElementSibling.textContent = "Please enter your name.";
+        isValid = false;
+    }
+    if (email.value.trim() === "") {
+        email.nextElementSibling.textContent = "Please enter your email.";
+        isValid = false;
+    } else if (!email.value.includes("@")) {
+        email.nextElementSibling.textContent = "Please enter valid email.";
+        isValid = false;
+    }
+    if (message.value.trim() === "") {
+        message.nextElementSibling.textContent = "Please enter your message.";
+        isValid = false;
+    } else if (message.value.trim().length < 10) {
+        message.nextElementSibling.textContent = 
+        "Message must be at least 10 characters.";
+        isValid = false;
+    }
+    if (isValid) {
+        document.querySelector("#form-status").textContent = 
+        "Message submitted successfully";
+
+        contactForm.reset();
+    }
+});
